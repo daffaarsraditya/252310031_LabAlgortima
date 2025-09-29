fe831f2a646f4b97e8350ee1850b307148d32639
Dapoyyy <daffaarsraditya@gmail.com>
2025-09-29 23:12:21 +0700

    Tugas Lab Algoritma

---
+++ Soal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A,B,C,D;
+    cout << "Nomor 1 - Penjumlahan" ;
+    cout << "\nMasukkan 2 angka" ;
+    cout << "\nA = " ;
+    cin >> A;
+    cout << "B = " ;
+    cin >> B;
+    cout << "Hasil A + B = " << A + B << endl ;
+
+    cout << "\nNomor 2 - Pengurangan" ;
+    cout << "\nMasukkan 2 angka" ;
+    cout << "\nC = " ;
+    cin >> C;
+    cout << "A = " ;
+    cin >> A;
+    cout << "Hasil C - A  = " << C - A << endl ;
+
+    cout << "\nNomor 3 - Perkalian" ;
+    cout << "\nMasukkan 2 angka" ;
+    cout << "\nD = " ;
+    cin >> D;
+    cout << "A = " ;
+    cin >> A;
+    cout << "Hasil D * A  = " << D * A << endl ;
+
+    return 0;
+}
