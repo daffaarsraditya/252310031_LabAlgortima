d76f194729c0310173fbff75968a8058757509a1
Dapoyyy <daffaarsraditya@gmail.com>
2025-12-02 12:48:03 +0700

    Add files via upload

---
+++ Pertemuan 8/Tugas 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    char kata[] = "FAKULTAS PARIWISATA DAN TEKNOLOGI INFORMASI";
+    int panjang = strlen(kata);
+    cout << "Panjang kata adalah: " << panjang << endl;
+    
+ return 0;
+}
---
+++ Pertemuan 8/Tugas 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    char kalimat[] = "Teknik";
+    strcat(kalimat, " Informatika");
+    
+    cout << kalimat;
+    
+    return 0;
+}
