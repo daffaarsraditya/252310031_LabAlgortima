27a9f26ab9b0a9e3e041af8659ef05deb62ff44e
Dapoyyy <daffaarsraditya@gmail.com>
2026-04-24 19:23:56 +0700

    Add files via upload

---
+++ Semester 2/setter and getter.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Karyawan {
+private:
+    string nama;
+    int umur;
+    string jabatan;
+
+public:
+    void setNama(string n) {
+        nama = n;
+    }
+
+    string getNama() {
+        return nama;
+    }
+
+    void setUmur(int u) {
+        umur = u;
+    }
+
+    int getUmur() {
+        return umur;
+    }
+
+    void setJabatan(string j) {
+        jabatan = j;
+    }
+
+    string getJabatan() {
+        return jabatan;
+    }
+};
+
+int main() {
+    Karyawan kar;
+    kar.setNama("Ade Antoni");
+    kar.setUmur(30);
+    kar.setJabatan("Asisten Dosen");
+
+    cout << "Nama: " << kar.getNama() << endl;
+    cout << "Umur: " << kar.getUmur() << endl;
+    cout << "Jabatan: " << kar.getJabatan() << endl;
+
+    return 0;
+}
